a59b6f53c1f8abf6741cb972a37e043e2eee86b2
alex <43148847+MugssyBoy@users.noreply.github.com>
2022-01-07 13:30:40 +0800

    initial commit

---
+++ src/components/Chart.vue
@@ -0,0 +1,76 @@
+<template>
+    <canvas :id="identifier" :style="style"></canvas>
+</template>
+
+<script>
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js'
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+)
+
+export default {
+    name: "QChart",
+    props: ["identifier", "style", "type", "labels", "datasets", "options"],
+    mounted() {
+        const ctx = document.getElementById(this.identifier)
+        new Chart(ctx, {
+            type: this.type,
+            data: {
+                labels: this.labels,
+                datasets: this.datasets
+            },
+            options: this.options
+        })
+    }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-chart
+      identifier="myChart"
+      style="height:30vh; width: 100%"
+      :type="line"
+      :labels="labels"
+      :datasets="datasets"
+      :options="options"
+    />
+  </q-page>
+</template>
+
+<script>
+import QChart from 'components/Chart.vue'
+
+export default {
+  name: "PageIndex",
+  components: {
+    QChart
+  },
+  data() {
+    return {
+      line: "line",
+      labels: [0, 10, 5, 2, 20, 30, 45],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45]
+        }
+      ],
+      options: {}
+    }
+  }
+}
+// import { defineComponent } from 'vue'
+
+// export default defineComponent({
+//   name: 'PageIndex'
+// })
+</script>
